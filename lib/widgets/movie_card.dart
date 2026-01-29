d9ddffd8e841c51d1d8c9f615f50d214231a5a0e
melodynarzary <melodynarzary4@gmail.com>
2026-01-29 23:47:36 +0530

    Refactor MovieCard to improve navigation and visuals(added hero animation)
    Updated MovieCard to use MovieDetailScreen and added Hero widget for image transition.

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -10,27 +10,58 @@ class MovieCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      // to handle taps
       onTap: () {
+        // Navigator.push navigates to a new screen
         Navigator.push(
           context,
+          // MaterialPageRoute creates a route that transitions to the new screen
           MaterialPageRoute(
-            builder: (context) => MovieDetailsScreen(movie: movie),
+            builder: (context) => MovieDetailScreen(movie: movie),
           ),
         );
       },
       child: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-
+        elevation: 4,
+        clipBehavior: Clip.antiAlias, // put the image inside the card bounds
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            Expanded(child: Image.network(movie.imageUrl, fit: BoxFit.cover)),
-            Padding(
-              padding: EdgeInsetsGeometry.only(top: 8.0, bottom: 8.0),
-              child: Text(
-                movie.title,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+            // add Hero widget for smooth image transition
+            Hero(
+              tag: movie.imageUrl,
+              child: Image.network(movie.imageUrl, fit: BoxFit.cover),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.only(
+                  top: 5,
+                  bottom: 10,
+                  left: 4,
+                  right: 4,
+                ),
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black87,
+                      Colors.black54,
+                      Colors.transparent,
+                    ],
+                  ),
+                ),
+                child: Text(
+                  movie.title,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
               ),
             ),
           ],
