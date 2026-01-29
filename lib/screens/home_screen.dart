f66f63d9286d35466ef5d827b5ca0dff4bf99350
melodynarzary <melodynarzary4@gmail.com>
2026-01-29 23:49:17 +0530

    Refactor movie data and improve HomeScreen layout
    Updated movie details and descriptions for several films. Adjusted layout of the HomeScreen widget to include padding.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,67 +7,79 @@ final List<Movie> movies = [
     title: "The Dark Knight",
     imageUrl:
         "https://upload.wikimedia.org/wikipedia/en/1/1c/The_Dark_Knight_%282008_film%29.jpg",
+    releaseYear: "2008",
     description:
-        'Batman has a new foe, the Joker, who is an accomplished criminal hell-bent on decimating Gotham City. Together with Gordon and Harvey Dent, Batman struggles to thwart the Joker before it is too late.',
-    rating: 9.1,
-    genre: 'Crime thriller, neo-noir, and action drama',
-    releaseyear: 2008,
-    director: 'Christopher Nolan',
+        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    rating: 9.0,
+    genres: ["Action", "Crime", "Drama"],
+    director: "Christopher Nolan",
+    producer: "Emma Thomas",
+    duration: "2h 32m",
   ),
   Movie(
     title: "Inception",
     imageUrl:
         "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+    releaseYear: "2010",
     description:
-        'Cobb steals information from his targets by entering their dreams. He is wanted for his alleged role in his wifes murder and his only chance at redemption is to perform a nearly impossible task',
+        "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
     rating: 8.8,
-    genre: 'Science fiction action thriller',
-    releaseyear: 2010,
-    director: 'Christopher Nolan',
+    genres: ["Action", "Adventure", "Sci-Fi"],
+    director: "Christopher Nolan",
+    producer: "Emma Thomas",
+    duration: "2h 28m",
   ),
   Movie(
     title: "Interstellar",
     imageUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkshzpfsh566sD5Im2qzFxaxfNFFqSQAFZgg&s",
+    releaseYear: "2014",
     description:
-        'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
-    rating: 8.7,
-    genre: 'Hard science fiction, space epic, and dystopian drama',
-    releaseyear: 2014,
-    director: 'Christopher Nolan',
+        "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    rating: 8.6,
+    genres: ["Adventure", "Drama", "Sci-Fi"],
+    director: "Christopher Nolan",
+    producer: "Emma Thomas",
+    duration: "2h 49m",
   ),
   Movie(
     title: "The Matrix",
     imageUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZNl1EYS9z4PfNViNWMK_V5ZspshcWfksobw&s",
+    releaseYear: "1999",
     description:
-        'Neo, a computer programmer and hacker, has always questioned the reality of the world around him. His suspicions are confirmed when Morpheus, a rebel leader, contacts him and reveals the truth to him.',
+        "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
     rating: 8.7,
-    genre: 'Science Fiction Action ',
-    releaseyear: 1999,
-    director: 'Lana Wachowski and Lilly Wachowski',
+    genres: ["Action", "Sci-Fi"],
+    director: "Lana Wachowski, Lilly Wachowski",
+    producer: "Joel Silver",
+    duration: "2h 16m",
   ),
   Movie(
     title: "Toy Story",
     imageUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd1_9O7ZAMWdim0aZ3AGYpzugvq7EPOdJpHA&s",
+    releaseYear: "1995",
     description:
-        'Andys favourite toy, Woody, is worried that after Andy receives his birthday gift, a new toy called Buzz Lightyear, his importance may get reduced. He thus hatches a plan to eliminate Buzz.',
+        "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.",
     rating: 8.3,
-    genre: 'Fantasy-adventure comedy',
-    releaseyear: 1995,
-    director: 'John Lasseter',
+    genres: ["Animation", "Adventure", "Comedy"],
+    director: "John Lasseter",
+    producer: "Bonnie Arnold, Ralph Guggenheim",
+    duration: "1h 21m",
   ),
   Movie(
     title: "Mr. Robot",
     imageUrl:
         "https://m.media-amazon.com/images/M/MV5BOTg4NTBiZDAtZTc0YS00NzZlLTg4Y2ItNGQ3M2ZlMDM5MWQzXkEyXkFqcGc@._V1_.jpg",
+    releaseYear: "2015",
     description:
-        'Elliot, a cyber-security engineer suffering from anxiety, works for a corporation and hacks felons by night. Panic strikes him after Mr Robot, a cryptic anarchist, recruits him to ruin his company.',
+        "Elliot, a brilliant but highly unstable young cyber-security engineer and vigilante hacker, becomes a key figure in a complex game of global dominance when he and his shadowy allies try to take down the corrupt corporation he is paid to protect.Elliot, a brilliant but highly unstable young cyber-security engineer and vigilante hacker, becomes a key figure in a complex game of global dominance when he and his shadowy allies try to take down the corrupt corporation he is paid to protect.",
     rating: 8.5,
-    genre: 'Psychological techno-thriller and drama',
-    releaseyear: 2019,
-    director: 'Sam Esmail',
+    genres: ["Crime", "Drama", "Thriller"],
+    director: "Sam Esmail",
+    producer: "Christian Slater, Sam Esmail",
+    duration: "4 Seasons",
   ),
 ];
 
@@ -77,22 +89,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Movies"),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple[100],
-      ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // columns in the grid
-          childAspectRatio: 0.7, // width to height ratio of each grid item
-          crossAxisSpacing: 10, // horizontal space between grid items
-          mainAxisSpacing: 10, // vertical space between grid items
+      appBar: AppBar(title: const Text("Movies"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // columns in the grid
+            childAspectRatio: 0.7, // width to height ratio of each grid item
+            crossAxisSpacing: 10, // horizontal space between grid items
+            mainAxisSpacing: 10, // vertical space between grid items
+          ),
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            return MovieCard(movie: movies[index]);
+          },
         ),
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          return MovieCard(movie: movies[index]);
-        },
       ),
     );
   }
