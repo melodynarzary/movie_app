53fe88213f52f188a2b8657b13086b22ec0fe9b9
melodynarzary <melodynarzary4@gmail.com>
2026-01-29 23:48:29 +0530

    Enhance UI and added hero animation
    Refactor MovieDetailsScreen to MovieDetailScreen and update layout.

--- lib/screens/movie_details_screen.dart
+++ lib/screens/movie_details_screen.dart
@@ -1,92 +1,221 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:movie_app/models/movie.dart';
 
-class MovieDetailsScreen extends StatelessWidget {
-  final Movie movie;
+class MovieDetailScreen extends StatelessWidget {
+  final Movie movie; // import movie model
 
-  const MovieDetailsScreen({super.key, required this.movie});
+  const MovieDetailScreen({super.key, required this.movie}); // constructor
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(movie.title),
-        backgroundColor: Colors.deepPurple[100],
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 10),
-
-              Image.network(
-                movie.imageUrl,
-                width: 200,
-                height: 300,
-                fit: BoxFit.cover,
+      appBar: AppBar(title: Text(movie.title)), // AppBar with movie name
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Children of the column
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Hero(
+                  tag: movie.imageUrl,
+                  child: Image.network(
+                    movie.imageUrl,
+                    width: double.infinity,
+                    height: 350,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned.fill(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(
+                      color: Colors.black.withValues(
+                        alpha: 0.5,
+                      ), // optional dark tint
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Card(
+                    elevation: 8,
+                    clipBehavior: Clip.antiAlias,
+                    // add Hero widget for smooth image transition
+                    child: Image.network(
+                      // Display movie image
+                      movie.imageUrl,
+                      width: 190,
+                      height: 280,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.description,
+                    textAlign: TextAlign.justify,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Rating",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Row(
+                        children: [
+                          buildStarRating(movie.rating),
+                          const SizedBox(width: 8),
+                          Text(
+                            "${movie.rating.toStringAsFixed(1)}/10",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Duration",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        movie.duration,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Director",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        movie.director,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Producer",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        movie.producer,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Release Year",
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      Text(
+                        movie.releaseYear,
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: movie.genres.length,
+                      itemBuilder: (context, index) {
+                        return SizedBox(
+                          width: 120,
+                          child: Card(
+                            //elevation: 2,
+                            margin: const EdgeInsets.symmetric(horizontal: 6),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                                vertical: 8,
+                              ),
+                              child: Center(
+                                child: Text(
+                                  movie.genres[index],
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
-
-              const SizedBox(height: 10),
-              Text(
-                movie.title,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SizedBox(
+            height: 50,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: handle watch action
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
               ),
-
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(movie.description, textAlign: TextAlign.center),
+              child: const Text(
+                "Watch Now",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 30),
-
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "Rating: ",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text("${movie.rating}"),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "Genre: ",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text("${movie.genre}"),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "Release Year: ",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text("${movie.releaseyear}"),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                    "Director: ",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text("${movie.director}"),
-                ],
-              ),
-            ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildStarRating(double rating) {
+    final double starRating = rating / 2;
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        if (index + 1 <= starRating) {
+          return const Icon(Icons.star, color: Colors.amber);
+        } else if (index + 0.5 <= starRating) {
+          return const Icon(Icons.star_half, color: Colors.amber);
+        } else {
+          return const Icon(Icons.star_border, color: Colors.amber);
+        }
+      }),
     );
   }
 }
